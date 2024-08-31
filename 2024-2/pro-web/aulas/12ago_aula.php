19c976cdfd7dfa75e3bd7d1ff1bd6a2e3d3d98d8
Kayke <kayke.gsousa15@gmail.com>
2024-08-31 19:10:45 -0300

    Atividades - Programação Web 2024.2

---
+++ 2024-2/pro-web/aulas/12ago_aula.php
@@ -0,0 +1,14 @@
+<html> 
+	<head>
+		<title> Teste PHP </title>
+	</head>
+<!--O php tem início a partir da tag < ? php (sem espaços), se encerrando em ?>. Ela pode entrar em qualquer espaço, até dentro de uma tag -->
+	<body>
+		<?php
+		echo "Configuração do site em PHP.";
+	//O comando echo é aquele que mostra algo na tela, eqiuvalente ao print.
+			phpinfo();
+	//Todo comando terminado em () é uma função, ou seja, é predefinido. Para cada instrução/comando em php, se encerra com ;.
+		?>
+	</body>
+</html>
