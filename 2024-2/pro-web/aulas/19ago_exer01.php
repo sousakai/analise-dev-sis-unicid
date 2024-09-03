0e150e1b32a9f7094514f85adc7af6576c55383e
Kayke <kayke.gsousa15@gmail.com>
2024-09-02 23:18:19 -0300

    Atividade - Programação Web

--- 2024-2/pro-web/aulas/19ago_exer01.php
+++ 2024-2/pro-web/aulas/19ago_exer01.php
@@ -13,22 +13,22 @@
 		$valor02 = 2;
 		
 		$resultado = $valor01 + $valor02;
-		echo "O resultado da adição dos valores: " . $valor01 . "e" . $valor02 "é de: " . $resultado;
+		echo "O resultado da adição dos valores: " . $valor01 . "e" . $valor02 . "é de: " . $resultado;
 		
 		$resultado = $valor01 - $valor02;
-		echo "O resultado da subtração dos valores: " . $valor01 . "e" . $valor02 "é de: " . $resultado;
+		echo "O resultado da subtração dos valores: " . $valor01 . "e" . $valor02 . "é de: " . $resultado;
 		
 		$resultado = $valor01 * $valor02;
-		echo "O resultado da multiplicação dos valores: " . $valor01 . "e" . $valor02 "é de: " . $resultado;
+		echo "O resultado da multiplicação dos valores: " . $valor01 . "e" . $valor02 . "é de: " . $resultado;
 		
 		$resultado = $valor01 / $valor02;
-		echo "O resultado da divisão dos valores: " . $valor01 . "e" . $valor02 "é de: " . $resultado;
+		echo "O resultado da divisão dos valores: " . $valor01 . "e" . $valor02 . "é de: " . $resultado;
 		
 		$resultado = $valor01 % $valor02;
-		echo "O resto das divisões dos valores: " . $valor01 . "e" . $valor02 "é de: " . $resultado;
+		echo "O resto das divisões dos valores: " . $valor01 . "e" . $valor02 . "é de: " . $resultado;
 		
 		$resultado = $valor01 ** $valor02;
-		echo "O resultado da exponenciação dos valores: " . $valor01 . "e" . $valor02 "é de: " . $resultado;
+		echo "O resultado da exponenciação dos valores: " . $valor01 . "e" . $valor02 . "é de: " . $resultado;
 
 	?>
 	</body>
